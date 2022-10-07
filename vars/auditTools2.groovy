53afc39bec439f12d4d5710be8bac3af1133fafc
namaziane <41172295+namaziane@users.noreply.github.com>
2022-10-08 01:14:49 +0200

    Update auditTools2.groovy

--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -2,10 +2,9 @@ def call(Map config) {
     node {
       echo ${config.message}
       sh '''
-        git version
-        docker version
-        dotnet --list-sdks
-        dotnet --list-runtimes
+                  git version
+                  java -version
+                  python --version
       '''
     }
 }
